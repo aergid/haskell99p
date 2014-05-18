fa2b8254ea39b94666f237d886903985d9c0e416
veldar <some@mail.org>
2014-05-18 23:58:32 +0400

    Extra checks for Lists with QuickCheck

--- test/Questions/ListsSpec.hs
+++ test/Questions/ListsSpec.hs
@@ -1,7 +1,9 @@
 module Questions.ListsSpec where
 
-import Test.Hspec
-import Questions.Lists
+import           Control.Exception (evaluate)
+import           Questions.Lists
+import           Test.Hspec
+import           Test.QuickCheck
 
 spec :: Spec
 spec = do
@@ -9,10 +11,20 @@ spec = do
     it "should return last element in a list" $
       myLast [1,2,3] `shouldBe` (3::Int)
 
+    it "should throw an error on empty list" $
+     evaluate(myLast []) `shouldThrow` errorCall "Empty list has no last element"
+
+
   describe "last'" $ do
     it "should return last element in a list" $
       myLast [1,2,3,4] `shouldBe` (4::Int)
 
+  describe "last''" $ do
+    it "returns the last element of an *nonempty* list" $
+          property $ \x xs -> myLast'' (x:xs) == (last (x:xs) :: Int)
+
+    it "throws an exception if used with an empty list" $ do
+          evaluate (myLast'' []) `shouldThrow` anyException
 
 
 main :: IO ()
