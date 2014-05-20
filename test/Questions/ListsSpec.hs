f8320d269fc2e433750c618bb02aa847521bb268
veldar <some@mail.org>
2014-05-20 23:51:25 +0400

    [Spec] for function families -- no autorun, only runhaskell

--- test/Questions/ListsSpec.hs
+++ test/Questions/ListsSpec.hs
@@ -5,43 +5,46 @@ import           Questions.Lists
 import           Test.Hspec
 import           Test.QuickCheck
 
-spec :: Spec
-spec = do
-  describe "last" $ do
-    it "should return last element in a list" $
-      myLast [1,2,3] `shouldBe` (3::Int)
+specList :: [Spec]
+specList = do
+    myLastImpl <- [myLast, myLast', myLast'']
+    return $ describe "last" $ do
+        it "should return last element in a list" $
+             myLastImpl [1,2,3] `shouldBe` (3::Int)
 
-    it "should throw an error on empty list" $
-     evaluate(myLast []) `shouldThrow` errorCall "Empty list has no last element"
+    {-it "should throw an error on empty list" $-}
+     {-evaluate(myLast []) `shouldThrow` errorCall "Empty list has no last element"-}
 
 
-  describe "last'" $ do
-    it "should return last element in a list" $
-      myLast [1,2,3,4] `shouldBe` (4::Int)
+  {-describe "last'" $ do-}
+    {-it "should return last element in a list" $-}
+      {-myLast [1,2,3,4] `shouldBe` (4::Int)-}
 
-  describe "last''" $ do
-    it "returns the last element of an *nonempty* list" $
-          property $ \x xs -> myLast'' (x:xs) == (last (x:xs) :: Int)
+  {-describe "last''" $ do-}
+    {-it "returns the last element of an *nonempty* list" $-}
+          {-property $ \x xs -> myLast'' (x:xs) == (last (x:xs) :: Int)-}
 
-    it "throws an exception if used with an empty list" $ do
-          evaluate (myLast'' []) `shouldThrow` anyException
+    {-it "throws an exception if used with an empty list" $ do-}
+          {-evaluate (myLast'' []) `shouldThrow` anyException-}
 
-  
-  describe "at" $ do
-    it "should return at given index in list" $ 
-      element_at [1,2,3,4,5] 3 `shouldBe` (3::Int)
+  {-describe "at" $ do-}
+    {-it "should return at given index in list" $ -}
+      {-element_at [1,2,3,4,5] 3 `shouldBe` (3::Int)-}
 
-  describe "at'" $ do
-    it "should return at given index in list" $ 
-      element_at' [1,2,3,4,5] 4 `shouldBe` (4::Int)
+  {-describe "at'" $ do-}
+    {-it "should return at given index in list" $ -}
+      {-element_at' [1,2,3,4,5] 4 `shouldBe` (4::Int)-}
 
-  describe "at''" $ do
-    it "should return at given index in list" $ 
-      element_at'' [1,2,3,4,5] 4 `shouldBe` (4::Int)
+  {-describe "at''" $ do-}
+    {-it "should return at given index in list" $ -}
+      {-element_at'' [1,2,3,4,5] 4 `shouldBe` (4::Int)-}
 
-  describe "at'''" $ do
-    it "should return at given index in list" $ 
-      element_at''' [1,2,3,4,5] 4 `shouldBe` (4::Int)
+  {-describe "at'''" $ do-}
+    {-it "should return at given index in list" $ -}
+      {-element_at''' [1,2,3,4,5] 4 `shouldBe` (4::Integer)-}
 
-main :: IO ()
-main = hspec spec
+{-main :: IO ()-}
+{-main = hspec spec-}
+{-main = undefined-}
+
+main = foldr (>>) (return ()) $ map hspec specList
