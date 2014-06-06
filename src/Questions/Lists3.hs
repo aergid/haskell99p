a5f1b9919d0853feba2556292a37be6f32af2cac
veldar <some@mail.org>
2014-06-06 23:42:57 +0400

    Problem 27. Nice!

--- src/Questions/Lists3.hs
+++ src/Questions/Lists3.hs
@@ -2,8 +2,8 @@ module Questions.Lists2 where
 
 import           Control.Applicative
 import           Control.Monad       (liftM)
+import           Data.List           (nub, (\\))
 import           System.Random
-import  Data.List (nub)
 {--
  --P21> insertAt 'X' "abcd" 2
  --"aXbcd"
@@ -105,3 +105,38 @@ combinations xs n | n > length xs = []
                   | n == length xs = [xs]
 combinations (x:xs) n = combinationsWith x xs (n-1) ++ combinations xs n
                 where combinationsWith x xs = map (x:) . combinations xs
+
+{-
+ --Problem 27
+ --
+ --Group the elements of a set into disjoint subsets.
+ --
+ --a) In how many ways can a group of 9 people work in 3 disjoint subgroups of 2, 3 and 4 persons? Write a function that generates all the possibilities and returns them in a list.
+ --
+ --b) Generalize the above predicate in a way that we can specify a list of group sizes and the predicate will return a list of groups.
+ --
+ --Note that we do not want permutations of the group members; i.e. ((ALDO BEAT) ...) is the same solution as ((BEAT ALDO) ...). However, we make a difference between ((ALDO BEAT) (CARLA DAVID) ...) and ((CARLA DAVID) (ALDO BEAT) ...).
+ --
+ --You may find more about this combinatorial problem in a good book on discrete mathematics under the term "multinomial coefficients".
+ --
+ --Example in Haskell:ab
+ --
+ --P27> group [2,3,4] ["aldo","beat","carla","david","evi","flip","gary","hugo","ida"]
+ --[[["aldo","beat"],["carla","david","evi"],["flip","gary","hugo","ida"]],...]
+ --(altogether 1260 solutions)
+ --
+ --27> group [2,2,5] ["aldo","beat","carla","david","evi","flip","gary","hugo","ida"]
+ --[[["aldo","beat"],["carla","david"],["evi","flip","gary","hugo","ida"]],...]
+ --(altogether 756 solutions)
+-}
+
+groups :: (Eq a) => [Int] -> [a] -> [[[a]]]
+groups (a:b:c:[]) xs = combinations xs a >>= \g1 ->
+                        combinations (xs \\ g1) b >>= \g2 ->
+                          map (\g3->[g1, g2, g3]) $ combinations ((xs \\ g1) \\ g2) c
+
+groups' (a:b:c:[]) xs = do
+                        g1 <- combinations xs a
+                        g2 <- combinations (xs \\ g1) b
+                        g3 <- combinations ((xs \\ g1) \\ g2) c
+                        return [g1, g2, g3]
