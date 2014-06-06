14bee8cd94f46b0bdbd1723378732f2cbd82e3e4
veldar <some@mail.org>
2014-06-06 23:48:53 +0400

    Good extra for P27 generalization

--- src/Questions/Lists3.hs
+++ src/Questions/Lists3.hs
@@ -140,3 +140,21 @@ groups' (a:b:c:[]) xs = do
                         g2 <- combinations (xs \\ g1) b
                         g3 <- combinations ((xs \\ g1) \\ g2) c
                         return [g1, g2, g3]
+
+{- 
+ --Generalized version for any multinomal length
+ -- How to do it with Do?
+ -combination :: Int -> [a] -> [([a],[a])]
+ -combination 0 xs     = [([],xs)]
+ -combination n []     = []
+ -combination n (x:xs) = ts ++ ds
+ -  where
+ -    ts = [ (x:ys,zs) | (ys,zs) <- combination (n-1) xs ]
+ -    ds = [ (ys,x:zs) | (ys,zs) <- combination  n    xs ]
+ - 
+ -group :: [Int] -> [a] -> [[[a]]]
+ -group [] _ = [[]]
+ -group (n:ns) xs =
+ -    [ g:gs | (g,rs) <- combination n xs
+ -           ,  gs    <- group ns rs ]
+ -}
