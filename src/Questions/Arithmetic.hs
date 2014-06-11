942c51eff4d1ce7fc9bdb629b226c317fa2991a8
veldar <imr@yandex.ru>
2014-06-11 13:46:00 +0400

    Problem 31 solved

--- src/Questions/Arithmetic.hs
+++ src/Questions/Arithmetic.hs
@@ -11,5 +11,19 @@ module Questions.Arithmetic where
  --True
 -}
 
+primes :: [Integer]
+primes = map head $ iterate sieve [2..]
+
+sieve :: [Integer] -> [Integer]
+sieve (x:xs) = filter ((/=0) . (`mod` x)) xs
+
 isPrime :: Integer -> Bool
-isPrime = undefined
+isPrime x = all ((/=0) . (x `mod`)) $ takeWhile (<= candidateRoot) primes
+    where candidateRoot = floor . sqrt $ fromIntegral x
+
+{-
+ -isPrime' n | n < 4 = n /= 1 
+ -isPrime' n = all ((/=0) . mod n) $ takeWhile (<= m) candidates 
+ -        where candidates = (2:3:[x + i | x <- [6,12..], i <- [-1,1]])
+ -              m = floor . sqrt $ fromIntegral n
+ -}
